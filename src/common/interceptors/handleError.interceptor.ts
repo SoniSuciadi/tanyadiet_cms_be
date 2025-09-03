9478c9343e1eb8f5393dbcb05ee908fce0e5ebd8
Soni Suciadi <sonisuciadi@Sonis-MacBook-Pro.local>
2025-09-04 01:12:27 +0700

    Update error handling and improve order handling in multiple controllers

--- src/common/interceptors/handleError.interceptor.ts
+++ src/common/interceptors/handleError.interceptor.ts
@@ -1,8 +1,11 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { ErrorUniqueConstraint } from './handleErrorUniqueConstraint.interceptor';
-import { OrderNotFoundError } from './handleErrorOrderNotFound.interceptor';
-import { JsonWebTokenError } from 'jsonwebtoken';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
 
+// ErrorResponse interface yang lebih ketat
 export interface ErrorResponse {
   name: string;
   message: string;
@@ -15,34 +18,30 @@ export interface ErrorResponse {
   constraint?: string;
 }
 
-@Catch()
-export class HandleError implements ExceptionFilter {
-  catch(exception, host: ArgumentsHost) {
-    console.log('👻 ~ HandleError ~ catch ~ exception:', exception);
+@Catch(HttpException)
+export class HandleHttpError implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
+    const status = exception.getStatus();
+    const message = exception.message;
 
     const resError: ErrorResponse = {
-      name: exception?.response?.error || 'Internal Server Error',
-      message: exception?.response?.message || 'Internal Server Error',
-      statusCode: exception?.response?.statusCode || 500,
+      name: exception.constructor.name || 'Internal Server Error',
+      message: message || 'Internal Server Error',
+      statusCode: status || 500,
     };
 
-    if (exception instanceof OrderNotFoundError) {
-      resError.name = 'NotFoundError';
-      resError.message = exception.message;
-      resError.statusCode = exception.statusCode;
-      resError.detail = exception.details;
-    } else if (exception instanceof ErrorUniqueConstraint) {
-      resError.name = 'NotFoundError';
-      resError.message = exception.message;
-      resError.statusCode = exception.statusCode;
-      resError.detail = exception.details;
-    } else if (exception instanceof JsonWebTokenError) {
-      resError.name = 'Unauthorized';
-      resError.message = exception.message;
-      resError.statusCode = 401;
+    // Customize the error response
+    if (exception instanceof HttpException) {
+      const responseObj = exception.getResponse();
+      if (typeof responseObj === 'object') {
+        resError.detail = responseObj['message'] || responseObj['detail'];
+      }
+    } else {
+      resError.detail = message;
     }
+
     response.status(resError.statusCode).json(resError);
   }
 }
